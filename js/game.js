7c903bfb5893429e1939605bb69ef8916ecc0049
Sam Auciello <olleicua@gmail.com>
2016-08-04 21:54:16 -0400

    star points and the ability to play moves

--- js/game.js
+++ js/game.js
@@ -1,38 +1,64 @@
 (function() {
 
   window.Point = {
-    build: function() {
-      var p = Object.create(this);
-      p.neighbors = [];
-      p.value = 'empty';
-      return p;
+    build: function(board) {
+      var point = Object.create(this);
+      point.board = board;
+      point.neighbors = [];
+      point.value = 'empty';
+      return point;
     },
 
     connect: function(other) {
       this.neighbors.push(other);
       other.neighbors.push(this);
     },
+
+    addDOMLink: function($point) {
+      var that = this;
+
+      this.$point = $point;
+
+      $point.addEventListener('click', function() {
+        that.board.playMove(that);
+      });
+    },
+
+    setValue: function(value) {
+      this.value = value;
+      this.$point.classList.remove('black', 'white', 'empty');
+      this.$point.classList.add(value);
+    }
   };
 
-  window.Game = {
-    newBoard: function(size) {
-      var board = [];
+  window.Game = [];
 
-      var row = null;
-      var point = null;
-      for (var y = 0; y < size; y++) {
-        row = [];
-        board.push(row);
-        for (var x = 0; x < size; x++) {
-          point = Point.build();
-          row.push(point)
-          if (y > 0) point.connect(board[y - 1][x]);
-          if (x > 0) point.connect(board[y][x - 1]);
-        }
+  Game.build = function(size) {
+    var board = Object.create(this);
+
+    var row = null;
+    var point = null;
+    for (var y = 0; y < size; y++) {
+      row = [];
+      board.push(row);
+      for (var x = 0; x < size; x++) {
+        point = Point.build(board);
+        row.push(point)
+        if (y > 0) point.connect(board[y - 1][x]);
+        if (x > 0) point.connect(board[y][x - 1]);
       }
-
-      return board;
     }
+
+    board.turn = 'black';
+
+    return board;
+  };
+
+  Game.playMove = function(point) {
+    if (point.value !== 'empty') return;
+
+    point.setValue(this.turn);
+    this.turn = this.turn === 'black' ? 'white' : 'black';
   };
 
 })()
--- js/ui.js
+++ js/ui.js
@@ -45,22 +45,44 @@
     var $point = null;
 
     var cellWidth = ($board.offsetWidth - 20) / size
+    var starPositions = stars(size);
 
-    board = Game.newBoard(size);
+    board = Game.build(size);
 
     for (var y = 0; y < size; y++) {
       $row = document.createElement('div');
-      $row.className = 'row';
+      $row.classList.add('row');
       $row.style.height = cellWidth + 'px';
       for (var x = 0; x < size; x++) {
         $point = document.createElement('div');
-        $point.className = 'point';
+        $point.classList.add('point', 'empty');
+        if (starPositions.indexOf(x) !== -1 && starPositions.indexOf(y) !== -1) {
+          $point.classList.add('star');
+        }
         $point.style.width = cellWidth + 'px';
         $point.style.height = cellWidth + 'px';
-        board[y][x].DOMLink = $point;
+        board[y][x].addDOMLink($point);
         $row.appendChild($point);
       }
       $board.appendChild($row);
+    }
+  };
+
+  stars = function(size) {
+    if (size < 9) {
+      if (size % 2 === 1) {
+        return [(size - 1) / 2];
+      } else {
+        return [];
+      }
+    } else if (size < 13) {
+      return [3, size - 4];
+    } else {
+      if (size % 2 === 1) {
+        return [3, size - 4, (size - 1) / 2];
+      } else {
+        return [3, size - 4];
+      }
     }
   };
 
